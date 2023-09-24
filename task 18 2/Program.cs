fa960f4ed1b63dc358abca5fe0c5a38eddbb5502
Alexandr <chika431sis@yandex.ru>
2023-09-24 13:43:42 +0500

    task 24.09.2023

---
+++ task 18 2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+string QuarterTwoCoords(int quadr)
+{
+    if(quadr==1) return "x > 0 && y > 0";
+    if(quadr==2) return "x < 0 && y > 0";
+    if(quadr==3) return "x < 0 && y < 0";
+    if(quadr==4) return "x > 0 && y < 0";
+    return null;
+   
+    // if (x > 0 && y > 0) return 1;
+    // if (x < 0 && y > 0) return 2;
+    // if (x < 0 && y < 0) return 3;
+    // if (x > 0 && y < 0) return 4;
+    // return 0;
+}
+
+
+Console.WriteLine("Введите номер области");
+Console.Write("область:");
+
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string result = QuarterTwoCoords(quarter);
+
+Console.WriteLine((result != null) ? result : "user did not input quater number");
---
+++ task pifagor/Program.cs
@@ -0,0 +1,46 @@
+﻿
+// using Internal;
+
+// Math.Pow(4,6)
+// Math.Sqrt
+
+// doble d = 5.09999984343;
+// doble dRound = Math.Round(d, 2, MidpointRounding.ToZero);
+// Console.WriteLine(dRound);
+
+// Задача 21: Напишите программу, которая 
+// принимает на вход координаты двух точек и 
+// находит расстояние между ними в 2D 
+// пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+
+
+
+double Pifagor(int x1, int x2, int y1, int y2)
+{
+double result = Math.Sqrt(Math.Pow(x1-x2), 2) + Math.Pow ((y1-y2),2);
+return result;
+
+}
+
+Console.WriteLine("Введите координаты точек");
+Console.Write("x1:");
+
+int x1Cordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("x2:");
+
+int x2Cordinate = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("y1:");
+int y1Cordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("y2:");
+int y2Cordinate = Convert.ToInt32(Console.ReadLine());
+double res = Pifagor(x1Cordinate, x2Cordinate, y1Cordinate, y2Cordinate);
+
+
+Console.WriteLine($"{res}");
