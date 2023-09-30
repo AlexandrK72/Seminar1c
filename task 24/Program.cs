fc64d2191d4af29299bce5b1779d43dcc13cbf40
Alexandr <chika431sis@yandex.ru>
2023-09-30 13:38:06 +0500

    task 28

--- task 24/Program.cs
+++ task 24/Program.cs
@@ -25,11 +25,7 @@ int SumNumbers(int num)
 
 
 Console.WriteLine("Введте целое число");
-Задача 26: Напишите программу, которая принимает
-на вход число и выдаёт количество цифр в числе.
-456 -> 3
-78 -> 2
-89126 -> 5
+int number = Convert.ToInt32(Console.ReadLine());
 
 if (number < 1)
 {
