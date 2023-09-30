389b8c906918063f9e169763c4bf266a59e34a9d
Alexandr <chika431sis@yandex.ru>
2023-09-30 13:28:29 +0500

    task 28

--- task 24/Program.cs
+++ task 24/Program.cs
@@ -25,7 +25,11 @@ int SumNumbers(int num)
 
 
 Console.WriteLine("Введте целое число");
-int number = Convert.ToInt32(Console.ReadLine());
+Задача 26: Напишите программу, которая принимает
+на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
 
 if (number < 1)
 {
